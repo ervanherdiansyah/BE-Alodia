23d98a17936843ebfae24c5abe1548de3929679a
ervanherdiansyah <77282241+ervanherdiansyah@users.noreply.github.com>
2024-08-14 05:53:26 +0700

    update

--- app/Models/User.php
+++ app/Models/User.php
@@ -26,6 +26,7 @@ class User extends Authenticatable implements JWTSubject
         'referral',
         'first_buy_success',
         'first_order',
+        'fcm_token',
     ];
 
     /**
@@ -94,24 +95,28 @@ class User extends Authenticatable implements JWTSubject
     {
         return $this->hasMany(order::class,  "user_id");
     }
-    
+
     // komisi history table!
     // affiliator_id -> user_id
-    public function komisi_affiliator_id(){
+    public function komisi_affiliator_id()
+    {
         return $this->hasMany(UserKomisiHistory::class, "affiliator_id");
     }
 
     // affiliate_id -> user_id
-    public function komisi_affiliate_id(){
+    public function komisi_affiliate_id()
+    {
         return $this->hasMany(UserKomisiHistory::class, "affiliate_id");
     }
 
     // affiliator komisi poin table! (Model AffiliatorPoinHistory, table AffiliatorPoinHistory)
-    public function komisipoin_affiliate_id(){
+    public function komisipoin_affiliate_id()
+    {
         // affiliator_id -> user_id
         return $this->hasMany(AffiliatorPoinHistory::class,  "affiliate_id");
     }
-    public function komisipoin_affiliator_id(){
+    public function komisipoin_affiliator_id()
+    {
         // affiliate_id -> user_id
         return $this->hasMany(AffiliatorPoinHistory::class, "affiliator_id");
     }
