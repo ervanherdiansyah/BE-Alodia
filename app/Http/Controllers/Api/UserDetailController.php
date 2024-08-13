98cde0a5d7f4547be50a725c081790e6fe19f25a
ervanherdiansyah <77282241+ervanherdiansyah@users.noreply.github.com>
2024-08-13 23:20:49 +0700

    update

--- app/Http/Controllers/Api/UserDetailController.php
+++ app/Http/Controllers/Api/UserDetailController.php
@@ -47,7 +47,7 @@ class UserDetailController extends Controller
         try {
             //code...
             $user_id = User::where('id', Auth::user()->id)->first();
-            $useReferralByUser = UserDetails::with('users')->latest()->get();
+            $useReferralByUser = UserDetails::latest()->get();
             return response()->json(['data' => $useReferralByUser, 'message' => 'Success'], 200);
         } catch (\Throwable $th) {
             //throw $th;
