b1eeb777333261037b547baa487422f83b963e16
ervanherdiansyah <77282241+ervanherdiansyah@users.noreply.github.com>
2024-08-14 11:43:16 +0700

    update

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -287,7 +287,7 @@ class OrderController extends Controller
             $user = User::where('id', $user_id)->first();
             // order diri sendiri
 
-            $user_orders = Order::where('user_id', $user_id)->where('status', 'Paid')->get();
+            $user_orders = Order::where('user_id', $user_id)->get();
             $data_order_user = [];
             foreach ($user_orders as $order) {
                 $keterangan = [
