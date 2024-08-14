011817263d900096912f7e405e92e201b7dbaf08
ervanherdiansyah <77282241+ervanherdiansyah@users.noreply.github.com>
2024-08-14 17:37:30 +0700

    update user alamat

--- app/Http/Controllers/Api/AlamatController.php
+++ app/Http/Controllers/Api/AlamatController.php
@@ -12,7 +12,7 @@ class AlamatController extends Controller
     public function getAlamat()
     {
         try {
-            $alamat = UserAlamat::with('users', 'provinsi', 'kota')->get();
+            $alamat = UserAlamat::with('users', 'provinsi', 'kota', 'kecamatan')->get();
             return response()->json(['data' => $alamat, 'message' => 'success'], 200);
         } catch (\Throwable $th) {
             //throw $th;
@@ -23,7 +23,7 @@ class AlamatController extends Controller
     {
         try {
             $user_id = Auth::user()->id;
-            $alamat = UserAlamat::with('users', 'provinsi', 'kota')->where('user_id', $user_id)->get();
+            $alamat = UserAlamat::with('users', 'provinsi', 'kota', 'kecamatan')->where('user_id', $user_id)->get();
             return response()->json(['data' => $alamat, 'message' => 'Success'], 200);
         } catch (\Throwable $th) {
             //throw $th;
@@ -35,18 +35,18 @@ class AlamatController extends Controller
     {
         try {
             $user_id = Auth::user()->id;
-            $alamat = UserAlamat::with('users', 'provinsi', 'kota')->where('user_id', $user_id)->where('alamat_utama', true)->first();
+            $alamat = UserAlamat::with('users', 'provinsi', 'kota', 'kecamatan')->where('user_id', $user_id)->where('alamat_utama', true)->first();
             return response()->json(['data' => $alamat, 'message' => 'Success'], 200);
         } catch (\Throwable $th) {
             //throw $th;
             return response()->json(['message' => 'Internal Server Error'], 500);
         }
     }
-    
+
     public function getAlamatPenjual()
     {
         try {
-            $alamat = UserAlamat::with('users', 'provinsi', 'kota')->where('id', 3)->first();
+            $alamat = UserAlamat::with('users', 'provinsi', 'kota', 'kecamatan')->where('id', 3)->first();
             return response()->json(['data' => $alamat, 'message' => 'Success'], 200);
         } catch (\Throwable $th) {
             //throw $th;
@@ -62,35 +62,35 @@ class AlamatController extends Controller
                 'alamat_lengkap' => 'required',
                 'provinsi_id' => 'required|integer',
                 'kota_id' => 'required|integer',
-                'kecamatan' => 'required',
+                'kecamatan_id' => 'required',
                 'kelurahan' => 'required',
                 'kode_pos' => 'required',
             ]);
-            
+
             $user_id = Auth::user()->id;
-            
+
             if ($request->alamat_utama == 1) {
                 $previous_alamat_utama = UserAlamat::where('user_id', $user_id)->where('alamat_utama', true)->first();
                 $previous_alamat_utama->update([
                     'alamat_utama' => false
                 ]);
             }
-            
+
             $alamat = UserAlamat::create([
                 'user_id' => Auth::user()->id,
                 'alamat_lengkap' => $request->alamat_lengkap,
                 'provinsi_id' => $request->provinsi_id,
                 'kota_id' => $request->kota_id,
-                'kecamatan' => $request->kecamatan,
+                'kecamatan_id' => $request->kecamatan_id,
                 'kelurahan' => $request->kelurahan,
                 'kode_pos' => $request->kode_pos,
                 'alamat_utama' => $request->alamat_utama,
                 'nama' => $request->nama,
                 'no_wa' => $request->no_wa,
-                
+
             ]);
-            
-            
+
+
 
             return response()->json(['data' => $alamat, 'message' => 'Success'], 200);
         } catch (\Throwable $th) {
@@ -107,14 +107,14 @@ class AlamatController extends Controller
                 'alamat_lengkap' => 'required',
                 'provinsi_id' => 'required|integer',
                 'kota_id' => 'required|integer',
-                'kecamatan' => 'required',
+                'kecamatan_id' => 'required',
                 'kelurahan' => 'required',
                 'kode_pos' => 'required',
                 'alamat_utama' => 'integer'
             ]);
 
             $user_id = Auth::user()->id;
-            $data = UserAlamat::where('id', $id)->where('user_id',$user_id)->first();
+            $data = UserAlamat::where('id', $id)->where('user_id', $user_id)->first();
 
             if ($user_id != $data->user_id) {
                 return response()->json(['message' => 'Tidak Bisa Mengubah Data Orang Lain'], 401);
@@ -131,7 +131,7 @@ class AlamatController extends Controller
                     'alamat_lengkap' => $request->alamat_lengkap,
                     'provinsi_id' => $request->provinsi_id,
                     'kota_id' => $request->kota_id,
-                    'kecamatan' => $request->kecamatan,
+                    'kecamatan_id' => $request->kecamatan_id,
                     'kelurahan' => $request->kelurahan,
                     'kode_pos' => $request->kode_pos,
                     'nama' => $request->nama,
@@ -147,7 +147,7 @@ class AlamatController extends Controller
                 'alamat_lengkap' => $request->alamat_lengkap,
                 'provinsi_id' => $request->provinsi_id,
                 'kota_id' => $request->kota_id,
-                'kecamatan' => $request->kecamatan,
+                'kecamatan_id' => $request->kecamatan_id,
                 'kelurahan' => $request->kelurahan,
                 'kode_pos' => $request->kode_pos,
                 'nama' => $request->nama,
